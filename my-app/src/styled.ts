e4e6ee60b6d715a0e46b9522b17f7b58f5f2f6cf
Luzhinskii Artem <128407406+KisliyBlin4ik@users.noreply.github.com>
2023-08-28 21:29:17 +0200

    Update styled.ts
    added destructuring and removed unnecessary className

--- my-app/src/styled.ts
+++ my-app/src/styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
-export const StyledWrapper = styled.div<{className: 'wrapper', theme: 'light' | 'dark'}>`
-background: ${(props: any) => props.theme === 'dark' ? '#1a1a1a' : '#fff'};
+export const StyledWrapper = styled.div<{ theme: 'light' | 'dark' }>`
+background: ${({ theme }) => theme === 'dark' ? '#1a1a1a' : '#fff'};
 `
 
 export const StyledContainer = styled.div<{theme: 'light' | 'dark'}>`
@@ -14,8 +14,8 @@ width: 100%;
 gap: 30px;
 padding: 30px;
 border: 1px solid rgb(210, 209, 209);
-background: ${(props: any) => props.theme === 'dark' ? '#1a1a1a' : '#fff'};
-color: ${(props: any) => props.theme === 'dark' ? '#fff' : '#111'};
+background: ${({ theme }) => theme === 'dark' ? '#1a1a1a' : '#fff'};
+color: ${({ theme }) => theme === 'dark' ? '#fff' : '#111'};
 `
 export const StyledForm = styled.form<{theme: 'light' | 'dark'}>`
 font-size: 20px;
@@ -28,10 +28,10 @@ width: 100%;
 gap: 30px;
 padding: 30px;
 border: 1px solid rgb(210, 209, 209);
-background: ${(props: any) => props.theme === 'dark' ? '#1a1a1a' : 'rgb(242, 242, 242)'};
-color: ${(props: any) => props.theme === 'dark' ? '#fff' : '#111'};
+background: ${({ theme }) => theme === 'dark' ? '#1a1a1a' : 'rgb(242, 242, 242)'};
+color: ${({ theme }) => theme === 'dark' ? '#fff' : '#111'};
 `
 
-export const StyledPageTemplate = styled.div<{className: 'PageTemplate', theme: 'light' | 'dark'}>`
-color: ${(props: any) => props.theme === 'dark' ? '#fff' : '#1a1a1a'};
+export const StyledPageTemplate = styled.div<{ theme: 'light' | 'dark' }>`
+color: ${({ theme }) => theme === 'dark' ? '#fff' : '#1a1a1a'};
 `
